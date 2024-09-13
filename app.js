a4b32a6ea1154432d0b8964ad40ecd7991c60db4
zakhar <zahar.ava@mail.ru>
2024-09-13 16:07:39 +0800

    small changes

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-'use strict'
+"use strict";
 
-const path = require('path')
-const AutoLoad = require('@fastify/autoload')
-const fastifyStatic = require('@fastify/static');
+const path = require("path");
+const AutoLoad = require("@fastify/autoload");
+const fastifyStatic = require("@fastify/static");
 
 module.exports = async function (fastify, opts) {
   // Place here your custom code!
 
   fastify.register(fastifyStatic, {
-    root: `${process.cwd()}/my-app/build`,
+    root: `${process.cwd()}/my-app/dist`,
   });
 
   fastify.setNotFoundHandler((req, res) => {
-    res.sendFile('index.html');
+    res.sendFile("index.html");
   });
 
   // Do not touch the following lines
@@ -21,14 +21,14 @@ module.exports = async function (fastify, opts) {
   // those should be support plugins that are reused
   // through your application
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
   // This loads all plugins defined in routes
   // define your routes in one of these
   fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+    dir: path.join(__dirname, "routes"),
+    options: Object.assign({}, opts),
+  });
+};
