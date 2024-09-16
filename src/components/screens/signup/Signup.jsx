d8fd6930bfa199f1955daefb3ac980eec034b465
zakhar <zahar.ava@mail.ru>
2024-09-16 14:24:58 +0800

    fix path

--- src/components/screens/signup/Signup.jsx
+++ src/components/screens/signup/Signup.jsx
@@ -1,5 +1,5 @@
-import Button from "../../ui/button/Button.jsx";
-import Field from "../../ui/Fields/Field.jsx";
+import Button from "../../ui/button/Button";
+import Field from "../../ui/Fields/Field";
 import Loader from "../../ui/Loader.jsx";
 import { useSignUp } from "./useSignup.js";
 
